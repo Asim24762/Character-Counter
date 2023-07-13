965d4a54502c47947d41bf55ab95f3c9f3bf2279
Muhammad Asim <128361114+Asim24762@users.noreply.github.com>
2023-07-12 17:26:59 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+let textBox = document.querySelector(".textBox");
+let countNum = document.querySelector(".count");
+let text = document.querySelector(".text");
+ 
+
+textBox.addEventListener("input",count);
+
+function count (){
+    let number = textBox .value.length;
+    countNum.textContent = number;
+    if (number <=150){
+text.classList.remove("active")
+}else{
+        text.classList.add("active");
+}
+}
